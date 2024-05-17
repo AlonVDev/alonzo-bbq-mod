5493b11541ccb624916ab188e0cf7534e19b3d7e
Alonzo <94796198+AlonzoVR@users.noreply.github.com>
2024-05-17 16:38:44 +0300

    Campfires now don't spawn lit, you have to use flint n steel

--- src/main/java/com/alonzovr/bbqmod/mixin/CampfireBlockMixin.java
+++ src/main/java/com/alonzovr/bbqmod/mixin/CampfireBlockMixin.java
@@ -1,4 +1,32 @@
 package com.alonzovr.bbqmod.mixin;
 
-public class CampfireBlockMixin {
+import net.minecraft.block.*;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+
+import static net.minecraft.block.CampfireBlock.*;
+
+@Mixin(CampfireBlock.class)
+public abstract class CampfireBlockMixin extends BlockWithEntity
+        implements Waterloggable {
+    protected CampfireBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    @Nullable
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockPos blockPos;
+        World worldAccess = ctx.getWorld();
+        boolean bl = worldAccess.getFluidState(blockPos = ctx.getBlockPos()).getFluid() == Fluids.WATER;
+        return (BlockState)((BlockState)((BlockState)((BlockState)this.getDefaultState().with(WATERLOGGED, bl)).with(SIGNAL_FIRE, this.isSignalFireBaseBlock(worldAccess.getBlockState(blockPos.down())))).with(LIT, bl)).with(FACING, ctx.getHorizontalPlayerFacing());
+    }
+
+    private boolean isSignalFireBaseBlock(BlockState state) {
+        return state.isOf(Blocks.HAY_BLOCK);
+    }
 }
