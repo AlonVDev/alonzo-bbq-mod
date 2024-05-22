57ca9153b43186d0edda12dbd6c16077b23308f3
Alonzo <94796198+AlonzoVR@users.noreply.github.com>
2024-05-22 21:06:24 +0300

    Overcooked Foods

--- src/main/java/com/alonzovr/bbqmod/item/ModFoodComponents.java
+++ src/main/java/com/alonzovr/bbqmod/item/ModFoodComponents.java
@@ -5,5 +5,5 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent OVERCOOKED_BEEF = new FoodComponent.Builder().hunger(5).saturationModifier(0.5f).meat().statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 600), 0.8f).build();
+    public static final FoodComponent OVERCOOKED_BEEF = new FoodComponent.Builder().hunger(5).saturationModifier(0.5f).meat().statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 400), 0.5f).build();
 }
