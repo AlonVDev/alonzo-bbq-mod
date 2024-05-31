5f9a830836744804f0a7ab861bfde6ad983447a6
Alonzo <94796198+AlonzoVR@users.noreply.github.com>
2024-05-31 14:05:44 +0300

    Sauces

--- src/main/java/com/alonzovr/bbqmod/item/ModFoodComponents.java
+++ src/main/java/com/alonzovr/bbqmod/item/ModFoodComponents.java
@@ -6,5 +6,6 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent SAUCED_BEEF = new FoodComponent.Builder().hunger(4).saturationModifier(0.4f).meat().build();
+    public static final FoodComponent COOKED_SAUCED_BEEF = new FoodComponent.Builder().hunger(10).saturationModifier(1.0f).meat().build();
     public static final FoodComponent SAUCE_BOTTLE = new FoodComponent.Builder().hunger(4).saturationModifier(0.1f).build();
 }
