3fc5dc1a0cffe43667a3279c9aa6bedaa36a377c
Alonzo <94796198+AlonzoVR@users.noreply.github.com>
2024-06-03 12:11:57 +0300

    Data gen

---
+++ src/main/java/com/alonzovr/bbqmod/datagen/ModModelProvider.java
@@ -0,0 +1,35 @@
+package com.alonzovr.bbqmod.datagen;
+
+import com.alonzovr.bbqmod.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.BARBECUE_BOTTLE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.KETCHUP_BOTTLE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.MUSTARD_BOTTLE, Models.GENERATED);
+
+        itemModelGenerator.register(ModItems.BARBECUE_BEEF, Models.GENERATED);
+        itemModelGenerator.register(ModItems.COOKED_BARBECUE_BEEF, Models.GENERATED);
+        itemModelGenerator.register(ModItems.KETCHUP_BEEF, Models.GENERATED);
+        itemModelGenerator.register(ModItems.COOKED_KETCHUP_BEEF, Models.GENERATED);
+        itemModelGenerator.register(ModItems.BARBECUE_PORKCHOP, Models.GENERATED);
+        itemModelGenerator.register(ModItems.COOKED_BARBECUE_PORKCHOP, Models.GENERATED);
+        itemModelGenerator.register(ModItems.KETCHUP_PORKCHOP, Models.GENERATED);
+        itemModelGenerator.register(ModItems.COOKED_KETCHUP_PORKCHOP, Models.GENERATED);
+    }
+}
