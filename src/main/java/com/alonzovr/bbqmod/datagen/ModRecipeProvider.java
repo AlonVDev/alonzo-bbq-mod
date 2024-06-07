c08889c5659edd4b382ce3bdab1edb806797773d
Alonzo <94796198+AlonzoVR@users.noreply.github.com>
2024-06-07 20:45:48 +0300

    LOTS of new cooking recipes for smelting and smoking

--- src/main/java/com/alonzovr/bbqmod/datagen/ModRecipeProvider.java
+++ src/main/java/com/alonzovr/bbqmod/datagen/ModRecipeProvider.java
@@ -4,8 +4,12 @@ import com.alonzovr.bbqmod.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.book.RecipeCategory;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
@@ -15,19 +19,52 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
-        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.BARBECUE_BEEF, ModItems.COOKED_BARBECUE_BEEF, 0.35F);
-        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.KETCHUP_BEEF, ModItems.COOKED_KETCHUP_BEEF, 0.35F);
+        //Campfire/Grill
+        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.BARBECUE_BEEF, ModItems.COOKED_BARBECUE_BEEF, 0.35f);
+        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.KETCHUP_BEEF, ModItems.COOKED_KETCHUP_BEEF, 0.35f);
 
-        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.BARBECUE_PORKCHOP, ModItems.COOKED_BARBECUE_PORKCHOP, 0.35F);
-        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.KETCHUP_PORKCHOP, ModItems.COOKED_KETCHUP_PORKCHOP, 0.35F);
+        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.BARBECUE_PORKCHOP, ModItems.COOKED_BARBECUE_PORKCHOP, 0.35f);
+        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.KETCHUP_PORKCHOP, ModItems.COOKED_KETCHUP_PORKCHOP, 0.35f);
 
-        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.BARBECUE_MUTTON, ModItems.COOKED_BARBECUE_MUTTON, 0.35F);
-        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.KETCHUP_MUTTON, ModItems.COOKED_KETCHUP_MUTTON, 0.35F);
+        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.BARBECUE_MUTTON, ModItems.COOKED_BARBECUE_MUTTON, 0.35f);
+        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.KETCHUP_MUTTON, ModItems.COOKED_KETCHUP_MUTTON, 0.35f);
 
-        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.BARBECUE_CHICKEN, ModItems.COOKED_BARBECUE_CHICKEN, 0.35F);
-        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.KETCHUP_CHICKEN, ModItems.COOKED_KETCHUP_CHICKEN, 0.35F);
+        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.BARBECUE_CHICKEN, ModItems.COOKED_BARBECUE_CHICKEN, 0.35f);
+        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.KETCHUP_CHICKEN, ModItems.COOKED_KETCHUP_CHICKEN, 0.35f);
 
-        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.BARBECUE_RABBIT, ModItems.COOKED_BARBECUE_RABBIT, 0.35F);
-        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.KETCHUP_RABBIT, ModItems.COOKED_KETCHUP_RABBIT, 0.35F);
+        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.BARBECUE_RABBIT, ModItems.COOKED_BARBECUE_RABBIT, 0.35f);
+        offerFoodCookingRecipe(exporter, "campfire_cooking", RecipeSerializer.CAMPFIRE_COOKING, 600, ModItems.KETCHUP_RABBIT, ModItems.COOKED_KETCHUP_RABBIT, 0.35f);
+
+        //Smelting
+        offerSmelting(exporter, Collections.singletonList(ModItems.BARBECUE_BEEF) , RecipeCategory.FOOD,ModItems.COOKED_BARBECUE_BEEF, 0.35f, 200, "");
+        offerSmelting(exporter, Collections.singletonList(ModItems.KETCHUP_BEEF) , RecipeCategory.FOOD,ModItems.COOKED_KETCHUP_BEEF, 0.35f, 200, "");
+
+        offerSmelting(exporter, Collections.singletonList(ModItems.BARBECUE_PORKCHOP) , RecipeCategory.FOOD,ModItems.COOKED_BARBECUE_PORKCHOP, 0.35f, 200, "");
+        offerSmelting(exporter, Collections.singletonList(ModItems.KETCHUP_PORKCHOP) , RecipeCategory.FOOD,ModItems.COOKED_KETCHUP_PORKCHOP, 0.35f, 200, "");
+
+        offerSmelting(exporter, Collections.singletonList(ModItems.BARBECUE_MUTTON) , RecipeCategory.FOOD,ModItems.COOKED_BARBECUE_MUTTON, 0.35f, 200, "");
+        offerSmelting(exporter, Collections.singletonList(ModItems.KETCHUP_MUTTON) , RecipeCategory.FOOD,ModItems.COOKED_KETCHUP_MUTTON, 0.35f, 200, "");
+
+        offerSmelting(exporter, Collections.singletonList(ModItems.BARBECUE_CHICKEN) , RecipeCategory.FOOD,ModItems.COOKED_BARBECUE_CHICKEN, 0.35f, 200, "");
+        offerSmelting(exporter, Collections.singletonList(ModItems.KETCHUP_CHICKEN) , RecipeCategory.FOOD,ModItems.COOKED_KETCHUP_CHICKEN, 0.35f, 200, "");
+
+        offerSmelting(exporter, Collections.singletonList(ModItems.BARBECUE_RABBIT) , RecipeCategory.FOOD,ModItems.COOKED_BARBECUE_RABBIT, 0.35f, 200, "");
+        offerSmelting(exporter, Collections.singletonList(ModItems.KETCHUP_RABBIT) , RecipeCategory.FOOD,ModItems.COOKED_KETCHUP_RABBIT, 0.35f, 200, "");
+
+        //Smoking
+        offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.BARBECUE_BEEF, ModItems.COOKED_BARBECUE_BEEF, 0.35f);
+        offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.KETCHUP_BEEF, ModItems.COOKED_KETCHUP_BEEF, 0.35f);
+
+        offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.BARBECUE_PORKCHOP, ModItems.COOKED_BARBECUE_PORKCHOP, 0.35f);
+        offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.KETCHUP_PORKCHOP, ModItems.COOKED_KETCHUP_PORKCHOP, 0.35f);
+
+        offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.BARBECUE_MUTTON, ModItems.COOKED_BARBECUE_MUTTON, 0.35f);
+        offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.KETCHUP_MUTTON, ModItems.COOKED_KETCHUP_MUTTON, 0.35f);
+
+        offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.BARBECUE_CHICKEN, ModItems.COOKED_BARBECUE_CHICKEN, 0.35f);
+        offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.KETCHUP_CHICKEN, ModItems.COOKED_KETCHUP_CHICKEN, 0.35f);
+
+        offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.BARBECUE_RABBIT, ModItems.COOKED_BARBECUE_RABBIT, 0.35f);
+        offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.KETCHUP_RABBIT, ModItems.COOKED_KETCHUP_RABBIT, 0.35f);
     }
 }
