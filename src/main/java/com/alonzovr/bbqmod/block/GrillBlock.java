36de8b261cd70fbd4b5793811e4023b1883d9768
Alonzo <94796198+AlonzoVR@users.noreply.github.com>
2024-06-12 12:45:29 +0300

    Random

--- src/main/java/com/alonzovr/bbqmod/block/GrillBlock.java
+++ src/main/java/com/alonzovr/bbqmod/block/GrillBlock.java
@@ -187,10 +187,6 @@ public class GrillBlock
         if (!state.get(LIT).booleanValue()) {
             return;
         }
-        /*if (state.get(WATERLOGGED)) {
-            world.setBlockState(pos, state.with(LIT, false));
-            return;
-        }*/
         if (random.nextInt(10) == 0) {
             world.playSound((double)pos.getX() + 0.5, (double)pos.getY() + 0.5, (double)pos.getZ() + 0.5, SoundEvents.BLOCK_CAMPFIRE_CRACKLE, SoundCategory.BLOCKS, 0.5f + random.nextFloat(), random.nextFloat() * 0.7f + 0.6f, false);
         }
