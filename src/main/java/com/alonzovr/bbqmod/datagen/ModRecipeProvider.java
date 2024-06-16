efd6c826930313225cfd3d9006a0658b7f5fa6c0
Alonzo <94796198+AlonzoVR@users.noreply.github.com>
2024-06-16 15:41:16 +0300

    New data gen recipes

--- src/main/java/com/alonzovr/bbqmod/datagen/ModRecipeProvider.java
+++ src/main/java/com/alonzovr/bbqmod/datagen/ModRecipeProvider.java
@@ -1,10 +1,16 @@
 package com.alonzovr.bbqmod.datagen;
 
+import com.alonzovr.bbqmod.block.ModBlocks;
 import com.alonzovr.bbqmod.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
 
 import java.util.function.Consumer;
 
@@ -62,5 +68,27 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
         offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.BARBECUE_RABBIT, ModItems.COOKED_BARBECUE_RABBIT, 0.35f);
         offerFoodCookingRecipe(exporter, "smoking", RecipeSerializer.SMOKING, 100, ModItems.KETCHUP_RABBIT, ModItems.COOKED_KETCHUP_RABBIT, 0.35f);
+
+        // Other
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.GRILL, 1)
+                .pattern("IBI")
+                .pattern("ICI")
+                .pattern("I I")
+                .input('I', Items.IRON_INGOT)
+                .input('B', Items.IRON_BARS)
+                .input('C', Items.COAL_BLOCK)
+                .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                .criterion(hasItem(Items.IRON_BARS), conditionsFromItem(Items.IRON_BARS))
+                .criterion(hasItem(Items.COAL_BLOCK), conditionsFromItem(Items.COAL_BLOCK))
+                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.GRILL)));
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.KETCHUP_BOTTLE)
+                .input(Items.GLASS_BOTTLE)
+                .input(Items.SUGAR)
+                .input(ModItems.TOMATO)
+                .criterion(hasItem(Items.GLASS_BOTTLE), conditionsFromItem(Items.GLASS_BOTTLE))
+                .criterion(hasItem(Items.SUGAR), conditionsFromItem(Items.SUGAR))
+                .criterion(hasItem(ModItems.TOMATO), conditionsFromItem(ModItems.TOMATO))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.KETCHUP_BOTTLE)));
     }
 }
