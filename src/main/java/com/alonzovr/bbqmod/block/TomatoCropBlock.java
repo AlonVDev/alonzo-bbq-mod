6ba8b4fa88ad3166250259732d8eff7d0c52a044
Alonzo <94796198+AlonzoVR@users.noreply.github.com>
2024-06-20 16:57:41 +0300

    Tomato Crop

--- src/main/java/com/alonzovr/bbqmod/block/TomatoCropBlock.java
+++ src/main/java/com/alonzovr/bbqmod/block/TomatoCropBlock.java
@@ -4,11 +4,10 @@ import com.alonzovr.bbqmod.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
-import net.minecraft.block.SweetBerryBushBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
@@ -18,15 +17,34 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 
 public class TomatoCropBlock extends CropBlock {
     public static final int MAX_AGE = 5;
     public static final IntProperty AGE = Properties.AGE_5;
+    private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{
+            //Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 2.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 4.0, 16.0),
+            //Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 6.0, 16.0),
+            //Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 9.0, 16.0),
+            //Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 12.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 14.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 14.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 14.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 14.0, 16.0)
+            };
 
     public TomatoCropBlock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return AGE_TO_SHAPE[this.getAge(state)];
     }
 
     @Override
@@ -51,12 +69,10 @@ public class TomatoCropBlock extends CropBlock {
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        boolean bl;
         int i = state.get(AGE);
-        boolean bl2 = bl = i == 5;
-        if (i > 4) {
+        if (i == 5) {
             int j = 1 + world.random.nextInt(2);
-            TomatoCropBlock.dropStack(world, pos, new ItemStack(ModItems.TOMATO, j + (bl ? 1 : 0)));
+            TomatoCropBlock.dropStack(world, pos, new ItemStack(ModItems.TOMATO, j));
             world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0f, 0.8f + world.random.nextFloat() * 0.4f);
             BlockState blockState = (BlockState)state.with(AGE, 2);
             world.setBlockState(pos, blockState, Block.NOTIFY_LISTENERS);
